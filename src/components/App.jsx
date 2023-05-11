459c4abdfb0098c4c70299c1014851a908a42c46
Ihor Karen <karenihor@gmail.com>
2023-05-11 17:29:09 +0300

    toastify & loadmore logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { Container } from './Container/Container.styled';
 import fetchImages from '../services/pixabayAPI';
 import SearchBar from './Searchbar';
@@ -11,6 +13,7 @@ export default function App() {
   const [value, setValue] = useState('');
   const [loading, setLoading] = useState(false);
   const [page, setPage] = useState(1);
+  const [loadmore, setLoadMore] = useState(true);
 
   useEffect(() => {
     const galleryList = document.querySelector('ul');
@@ -24,6 +27,10 @@ export default function App() {
         behavior: 'smooth',
       });
     }
+
+    if (images.length > 0) {
+      toast(`We found ${images.length} images for your request!`);
+    }
   }, [images]);
 
   async function getImages(searchValue) {
@@ -32,17 +39,31 @@ export default function App() {
 
       if (searchValue !== value) {
         const responseImages = await fetchImages(searchValue, 1);
+
+        if (responseImages.length === 0) {
+          setImages([]);
+          return toast(`Oops, no results were found for your search.`);
+        }
+
+        setLoadMore(true);
         setImages([...responseImages]);
         setValue(searchValue);
         setPage(1);
+        return;
       }
 
       if (searchValue === value) {
         const responseImages = await fetchImages(searchValue, page + 1);
-        setImages([...images, ...responseImages]);
-        return;
+
+        if (responseImages.length < 12) {
+          setLoadMore(false);
+        }
+
+        return setImages([...images, ...responseImages]);
       }
     } catch (error) {
+      setLoadMore(false);
+      toast(`All images found for your request!`);
     } finally {
       setLoading(false);
     }
@@ -53,7 +74,7 @@ export default function App() {
     getImages(value);
   }
 
-  const showLoadMoreButton = images.length < 12 || images.length === 0;
+  const showLoadMoreButton = images.length < 12 || !loadmore;
 
   return (
     <Container>
@@ -61,6 +82,7 @@ export default function App() {
       <SearchBar onSubmit={getImages} />
       <ImageGallery images={images} />
       {showLoadMoreButton ? null : <Button buttonClick={handleLoadMoreClick} />}
+      <ToastContainer autoClose={1500} />
     </Container>
   );
 }
